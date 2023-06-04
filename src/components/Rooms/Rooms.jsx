22a5173b4ca9b5f35b53e4facf03fb9e20ecb587
Kamrul Hasan <kamrul444216@gmail.com>
2023-06-05 01:49:47 +0600

    Footer added

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -3,6 +3,7 @@ import Container from "../Shared/Container";
 import Card from "./Card";
 import Loader from "../Shared/Loader";
 import { useSearchParams } from "react-router-dom";
+import Heading from "../Shared/Heading/Heading";
 
 const Rooms = () => {
   const [params, setParams] = useSearchParams();
@@ -34,11 +35,16 @@ const Rooms = () => {
 
   return (
     <Container>
+      {rooms && rooms.length>0 ? 
       <div className="pt-12 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-        {rooms.map((room, index) => (
-          <Card room={room} key={index} />
-        ))}
-      </div>
+      {rooms.map((room, index) => (
+        <Card room={room} key={index} />
+      ))}
+    </div> :
+    <div className="pt-12">
+        <Heading title={'No Room Available In This Category'} subtitle={'Please Select Other Categories'} center={true}/>
+    </div>
+    }
     </Container>
   );
 };
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import NavBar from "../components/Shared/NavBar/NavBar"
 import { Outlet } from "react-router-dom"
+import Footer from "../components/Shared/Footer/Footer"
 
 const Main = () => {
   return (
@@ -9,6 +10,7 @@ const Main = () => {
       <div className="pt-20 pb-20">
       <Outlet/>
       </div>
+      <Footer/>
     </div>
   )
 }
